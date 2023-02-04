2d3317e7faf95fc936d55cb97a40c8d5aa202a6a
Md Atiqur Rahman <ar.diu.cse@gmail.com>
2023-02-04 21:02:14 +0600

    fix: loader issue on product modal

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
