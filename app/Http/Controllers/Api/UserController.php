40148ccd421a67e267c47dcc9f0470021b3e2d71
Md Atiqur Rahman <ar.diu.cse@gmail.com>
2023-02-13 00:10:32 +0600

    feat: admin user crud complete

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,122 +3,64 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\ProductRequest;
-use App\Http\Resources\ProductListResource;
-use App\Http\Resources\ProductResource;
-use App\Models\Api\Product;
-use Exception;
-use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
-use Illuminate\Http\UploadedFile;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\URL;
-use Illuminate\Support\Str;
+use App\Http\Requests\CreateUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\Api\User;
+use Illuminate\Support\Facades\Hash;
 
-class ProductController extends Controller
+class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return AnonymousResourceCollection
-     */
-    public function index(): AnonymousResourceCollection
+    public function index()
     {
-        $search = request('search', false);
-        $per_page = request('per_page', 10);
+        $perPage = request('per_page', 10);
+        $search = request('search', '');
         $sortField = request('sort_field', 'updated_at');
         $sortDirection = request('sort_direction', 'desc');
 
-        $query = Product::query();
-        $query->orderBy($sortField, $sortDirection);
-        if ($search) {
-            $query->where('title', 'like', "%$search%")
-            ->orWhere('description', 'like', "%$search%");
-        }
-        return ProductListResource::collection($query->paginate($per_page));
+        $query = User::query()
+            ->orderBy($sortField, $sortDirection)
+            ->paginate($perPage);
+
+        return UserResource::collection($query);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param ProductRequest $request
-     * @return ProductResource
-     * @throws Exception
-     */
-    public function store(ProductRequest $request): ProductResource
+
+    public function store(CreateUserRequest $request)
     {
         $data = $request->validated();
+        $data['is_admin'] = true;
+        $data['email_verified_at'] = date('Y-m-d H:i:s');
+        $data['password'] = Hash::make($data['password']);
+
         $data['created_by'] = $request->user()->id;
         $data['updated_by'] = $request->user()->id;
 
-        $image = $data['image'] ?? null;
-        if ($image) {
-            $relativePath = $this->saveImage($image);
-            $data['image'] = URL::to(Storage::url($relativePath));
-            $data['image_mime'] = $image->getClientMimeType();
-            $data['image_size'] = $image->getSize();
-        }
+        $user = User::create($data);
 
-        $product = Product::create($data);
-        return new ProductResource($product);
+        return new UserResource($user);
     }
 
 
-    public function show(Product $product)
-    {
-        return new ProductResource($product);
-    }
-
-
-    public function update(ProductRequest $request, Product $product)
+    public function update(UpdateUserRequest $request, User $user)
     {
         $data = $request->validated();
+
+        if (!empty($data['password'])) {
+            $data['password'] = Hash::make($data['password']);
+        }
         $data['updated_by'] = $request->user()->id;
 
-        $image = $data['image'] ?? null;
+        $user->update($data);
 
-        if ($image) {
-            $relativePath = $this->saveImage($image);
-            $data['image'] = URL::to(Storage::url($relativePath));
-            $data['image_mime'] = $image->getClientMimeType();
-            $data['image_size'] = $image->getSize();
-        }
-
-        // if there is an old image, delete it
-        if ($product->image) {
-            Storage::deleteDirectory('/public/' . dirname($product->image));
-        }
-
-        $product->update($data);
-
-        return new ProductResource($product);
+        return new UserResource($user);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Product $product)
+
+    public function destroy(User $user)
     {
-        $product->delete();
+        $user->delete();
 
         return response()->noContent();
-    }
-
-    /**
-     * @throws Exception
-     */
-    private function saveImage(UploadedFile $image)
-    {
-        $path = 'images/' .Str::random();
-        if (!Storage::exists($path)) {
-            Storage::makeDirectory($path, 0755, true);
-        }
-
-        if (!Storage::putFileAs('public/' . $path, $image, $image->getClientOriginalName())) {
-            throw new Exception('Unable to save file \"' . $image->getClientOriginalName() . '\"');
-        }
-        return $path . '/' . $image->getClientOriginalName();
     }
 }
